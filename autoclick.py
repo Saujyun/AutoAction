271a00af9efab63310f98aac4143a38d6ef9a75d
“Saujyun <l>
2020-06-13 22:02:35 +0800

    添加华工报平安自动签到

--- autoclick.py
+++ autoclick.py
@@ -1,38 +1,37 @@
 # 方便延时加载
+import os
 import time
 from selenium import webdriver
- 
+
 # 模拟浏览器打开网站
-browser = webdriver.Chrome()
-browser.get('http://IamOK.scut.edu.cn')
+browser = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver")
+# browser.get('http://IamOK.scut.edu.cn')
+browser.get('https://sso.scut.edu.cn/cas/login?service=https%3A%2F%2Fiamok.scut.edu.cn%2Fcas%2Flogin')
 # 将窗口最大化
 browser.maximize_window()
- 
-# 根据路径找到按钮,并模拟进行点击
-browser.find_element_by_xpath('//*[@id="app"]/div/div/div[2]/div[3]/button').click()
-# 延时2秒，以便网页加载所有元素，避免之后找不到对应的元素
-time.sleep(5)
- 
+
 # 格式是PEP8自动转的
 # 这里是找到输入框,发送要输入的用户名和密码,模拟登陆
 browser.find_element_by_xpath(
-    "//*[@id='aw-login-user-name']").send_keys("账号")
+    "//*[@id='un']").send_keys(os.environ["SCUT_USER"])
 browser.find_element_by_xpath(
-    "//*[@id='aw-login-user-password']").send_keys("密码")
+    "//*[@id='pd']").send_keys(os.environ["SCUT_PASSWORD"])
 # 在输入用户名和密码之后,点击登陆按钮
-browser.find_element_by_xpath("//*[@id='login_submit']").click()
-time.sleep(2)
- 
-# 点击登陆后的页面中的签到,跳转到签到页面
-browser.find_element_by_xpath("/html/body/div[1]/div/div[5]/a").click()
-time.sleep(2)
- 
-# 点击签到,实现功能
-browser.find_element_by_xpath("//*[@id='qd_button']").click()
-time.sleep(2)
- 
+browser.find_element_by_xpath("//*[@id='index_login_btn']").click()
+time.sleep(5)
+
+
+if("今天已经填报过了哦" in browser.find_element_by_xpath("//*[@id='app']/div/div/div[2]/div[3]/div[1]/span").text ):
+    # 点击签到,实现功能
+    browser.find_element_by_xpath("//*[@id='app']/div/div/div[2]/div[3]/button").click()
+    time.sleep(2)
+    print("已经签到")
 # 这个print其实没事什么用,如果真的要测试脚本是否运行成功，可以用try来抛出异常
 print("签到成功")
- 
+
+# 保存email内容
+with open("email.txt", 'w',encoding="utf-8") as email:
+    email.writelines("签到成功!")
+
 # 脚本运行成功,退出浏览器
 browser.quit()
